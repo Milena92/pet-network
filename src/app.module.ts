529de3a35336df7238898a642119e7c4b4ccdb93
Milena92 <mimilazarevic1992@gmail.com>
2021-04-02 18:04:10 +0200

    project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './pet/pet.entity';
+import { PetModule } from './pet/pet.module';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [
+        PetModule,
+        UserModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'pet-network',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+    ],
 })
 export class AppModule {}
